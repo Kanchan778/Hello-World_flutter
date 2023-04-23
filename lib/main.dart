7ef52c8426a4484b0e2aac37c3d39a16962c2af9
Kanchan778 <106904293+Kanchan778@users.noreply.github.com>
2023-04-23 13:25:26 +0545

    Hello World

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:Scaffold(
+        body: Center(
+          child: Text("Hello World",style: TextStyle(fontSize: 80, color: Color.fromARGB(255, 121, 4, 141)),),
+        ),
+      ),
+       
+        
+
+
+      );
+      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    
+  }
+}
+
